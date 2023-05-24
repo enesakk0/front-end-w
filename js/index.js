02a5e5baff62854ef9dbf60f246ef91eadb93d00
Enes Akkaya <130901435+enesakk0@users.noreply.github.com>
2023-05-24 09:33:46 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 function closeNav() {
     document.getElementById("menu-icon-close").style.display = "none";
-    document.getElementById("header-mobile-menu").style.display = "none";
+    document.getElementById("header-mobile-menu").style.visibility = "hidden";
     document.getElementById("header-mobile-menu").style.left = "-100px";
     document.getElementById("header-mobile-menu").style.marginLeft = "500px";
     document.getElementById("header-mobile-menu").style.width = "0px"; 
+    document.getElementById("header-mobile-menu").style.fontSize = "0px";
 }
+
 
 function openNav() {
     document.getElementById("menu-icon-close").style.display = "block";
-    document.getElementById("header-mobile-menu").style.display = "block";
+    document.getElementById("header-mobile-menu").style.visibility = "visible";
     document.getElementById("header-mobile-menu").style.width = "300px";    
     document.getElementById("header-mobile-menu").style.left = "140px";
     document.getElementById("header-mobile-menu").style.marginLeft = "20px";
